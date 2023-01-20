f022b9d7786041c185de5a8ef5402492b5f86da2
Andrei Kaleshka <ka8725@gmail.com>
2023-01-20 23:15:40 +0100

    fixes

--- js/main.js
+++ js/main.js
@@ -1,24 +1,3 @@
 import '../css/main.scss';
 import './modules/burger-menu.js';
 import './modules/slider.js';
-
-export function initBackToTop() {
-  const backToTop = document.querySelector('#toTop');
-
-  window.addEventListener('scroll', () => {
-      if (window.pageYOffset > 300) {
-          backToTop.style.display = 'block';
-      } else {
-          backToTop.style.display = 'none';
-      }
-  });
-
-  backToTop.addEventListener('click', () => {
-      window.scrollTo({
-          top: 0,
-          left: 0,
-          behavior: 'smooth',
-      });
-  });
-}
-initBackToTop();
