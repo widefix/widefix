8f6c728347f4e35a2477e619ef0fbdc3e73c04e9
Andrei Kaleshka <ka8725@gmail.com>
2023-10-16 16:54:21 +0200

    rework landing

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const toggleButton = document.querySelector('.menu-toggle');
+const nav = document.querySelector('nav');
+
+toggleButton.addEventListener('click', () => {
+  nav.classList.toggle('visible');
+});
+
